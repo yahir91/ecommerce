20959730e2d2a7b8a8903a2da0d7ee6e2bff55f9
yahir91 <yahir.cardona@gmail.com>
2024-03-07 11:00:43 -0600

    feat: created enpoints for address

---
+++ src/api/address.ts
@@ -0,0 +1,87 @@
+import { authFetch } from "../utils/authFetch";
+import { ENV } from "../utils/constants";
+
+export class Address {
+  async create(data: any, userId: number) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.ADDRESS}`;
+      const params = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: {
+            ...data,
+            user: userId,
+          },
+        }),
+      };
+
+      const response: any = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAll(userId: number) {
+    try {
+      const filters = `filters[user][id][$eq]=${userId}`;
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.ADDRESS}?${filters}`;
+
+      const response: any = await authFetch(url);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(data: any, addressId: number) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.ADDRESS}/${addressId}`;
+      const params = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data }),
+      };
+
+      const response: any = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async delete(addressId: number) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.ADDRESS}/${addressId}`;
+      const params = {
+        method: "DELETE",
+      };
+
+      const response: any = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
