d9ac6cdef612951461ba8b878aa205270168ebd9
yahir91 <yahir.cardona@gmail.com>
2024-03-12 14:32:03 -0600

    feat: created wishlist endpoint and icon

---
+++ src/api/wishList.ts
@@ -0,0 +1,90 @@
+import { authFetch } from "../utils/authFetch";
+import { ENV } from "../utils/constants";
+
+export class Wishlist {
+  async check(userId: any, gameId: any) {
+    try {
+      const filterUser = `filters[user][id][$eq][0]=${userId}`;
+      const filterGame = `filters[game][id][$eq][1]=${gameId}`;
+      const urlParams = `${filterUser}&${filterGame}`;
+
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.WISHLIST}?${urlParams}`;
+
+      const response: any = await authFetch(url);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      if (result.data.length === 0) {
+        return false;
+      }
+
+      return result.data[0];
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async add(userId: any, gameId: any) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.WISHLIST}`;
+      const params = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: {
+            user: userId,
+            game: gameId,
+          },
+        }),
+      };
+
+      const response: any = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async delete(id: any) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.WISHLIST}/${id}`;
+      const params = {
+        method: "DELETE",
+      };
+
+      const response: any = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAll(userId: any) {
+    try {
+      const filters = `filters[user][id][$eq]=${userId}`;
+      const populate = "populate[0]=game&populate[1]=game.cover";
+      const urlParams = `${filters}&${populate}`;
+
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.WISHLIST}?${urlParams}`;
+      const response: any = await authFetch(url);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
---
+++ src/components/Shared/WishListIcon/WishListIcon.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { Icon } from "semantic-ui-react";
+import classNames from "classnames";
+import styles from "./WishlistIcon.module.scss";
+import { Wishlist } from "../../../api/wishList";
+import { useAuth } from "../../../hooks/useAuth";
+
+const wishlistCtrl = new Wishlist();
+const WishlistIcon = (props: any) => {
+  const { gameId, className, removeCallback } = props;
+  const [hasWishlist, setHasWishlist] = useState<any>(null);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await wishlistCtrl.check(user.id, gameId);
+        setHasWishlist(response);
+      } catch (error) {
+        setHasWishlist(false);
+        console.error(error);
+      }
+    })();
+  }, [gameId, user.id]);
+
+  const addWishlist = async () => {
+    const response = await wishlistCtrl.add(user.id, gameId);
+    setHasWishlist(response);
+  };
+
+  const deleteWishlist = async () => {
+    try {
+      await wishlistCtrl.delete(hasWishlist.id);
+      setHasWishlist(false);
+
+      if (removeCallback) {
+        removeCallback();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (hasWishlist === null) return null;
+
+  return (
+    <Icon
+      name={hasWishlist ? "heart" : "heart outline"}
+      onClick={hasWishlist ? deleteWishlist : addWishlist}
+      className={classNames(styles.wishlistIcon, {
+        [className]: className,
+      })}
+    />
+  );
+};
+
+export default WishlistIcon;
