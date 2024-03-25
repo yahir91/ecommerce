8229d5c0caeb770532c883076cad12b16c58f9cd
yahir91 <yahir.cardona@gmail.com>
2024-03-25 13:49:44 -0600

    wip: fixing server components

--- src/app/game/[game]/page.tsx
+++ src/app/game/[game]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { Game } from "../../api/game";
 import GamePage from "./GamePage";
+import { ENV } from "../../../utils/constants";
 
 type Props = {
   params: { game: string };
@@ -15,9 +16,19 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 const page = async ({ params }: { params: { game: string } }) => {
-  const gameCtrl = new Game();
-  const response = await gameCtrl.getBySlug(params.game);
-  return <GamePage data={response} />;
+  // const gameCtrl = new Game();
+  // const response = await gameCtrl.getBySlug(params.game);
+  const filters = `filters[slug][$eq]=${params.game}`;
+  const populate = `populate[0]=wallpaper&populate[1]=cover&populate&populate[2]=screenshots&populate[3]=platform&populate[4]=platform.icon`;
+  const url = `${ENV.API_URL}/${ENV.ENDPOINTS.GAME}?${filters}&${populate}`;
+
+  const response = await fetch(url);
+  const result = await response.json();
+
+  if (response.status !== 200) throw result;
+
+  // return result.data[0];
+  return <GamePage data={result.data[0]} />;
 };
 
 export default page;
