4d4fe1ce6f004dffa6aa91c41314256afad4167e
yahir91 <yahir.cardona@gmail.com>
2024-03-25 13:59:56 -0600

    wip: fixing server components

--- src/app/game/[game]/page.tsx
+++ src/app/game/[game]/page.tsx
@@ -1,19 +1,17 @@
-import { Metadata } from "next";
-import { Game } from "../../api/game";
-import GamePage from "./GamePage";
 import { ENV } from "../../../utils/constants";
+import GamePage from "./GamePage";
 
-type Props = {
-  params: { game: string };
-};
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const gameCtrl = new Game();
-  const response = await gameCtrl.getBySlug(params.game);
-  return {
-    title: response.attributes.title,
-    description: response.game?.attributes.summary,
-  };
-}
+// type Props = {
+//   params: { game: string };
+// };
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const gameCtrl = new Game();
+//   const response = await gameCtrl.getBySlug(params.game);
+//   return {
+//     title: response.attributes.title,
+//     description: response.game?.attributes.summary,
+//   };
+// }
 
 const page = async ({ params }: { params: { game: string } }) => {
   // const gameCtrl = new Game();
