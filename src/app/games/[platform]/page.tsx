75d01d1fa4371438a09aba401bce49cbe5e4544d
yahir91 <yahir.cardona@gmail.com>
2024-03-25 11:15:45 -0600

    fix: server and client components

--- src/app/games/[platform]/page.tsx
+++ src/app/games/[platform]/page.tsx
@@ -1,60 +1,40 @@
-"use client";
+import type { Metadata } from "next";
+import { ENV } from "../../../utils/constants";
+import PlatformPage from "./PlaftormPage";
 
-import { size } from "lodash";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Container } from "semantic-ui-react";
-import GridGames from "../../../components/Shared/GridGames/GridGames";
-import NoResult from "../../../components/Shared/NoResult/NoResult";
-import Pagination from "../../../components/Shared/Pagination/Pagination";
-import { Separator } from "../../../components/Shared/Separator/Separator";
-import { BasicLayout } from "../../../layouts/BasicLayout/BasicLayout";
+type Props = {
+  params: { platform: string };
+};
 
-const PlatformPage = ({ params }: { params: { platform: string } }) => {
-  const [data, setData] = useState<any>([]);
-  const searchParams = useSearchParams();
-  const page = searchParams.get("page") ?? "1";
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const response = await fetch(
+    `${ENV.URL}/api/games/${params.platform}`
+  );
+  const { data } = await response.json();
+  return {
+    title: data?.platform?.attributes.title,
+  };
+}
 
-  useEffect(() => {
-    const asyncFunction = (async () => {
-      const response = await fetch(
-        `../../api/games/${params.platform}?page=${page}`
-      );
-      const { data: parsedData } = await response.json();
-      setData(parsedData);
-    })();
-  }, [params.platform, page]);
+const page = async ({
+  params,
+  searchParams,
+}: {
+  params: { platform: string };
+  searchParams: { [page: string]: string | string[] | undefined };
+}) => {
+  const page = searchParams.page || "1";
 
-  const hasProducts = size(data.games) > 0;
+  const response = await fetch(
+    `${ENV.URL}/api/games/${params.platform}?page=${page}`
+  );
+  const { data } = await response.json();
 
   return (
-    <BasicLayout relative>
-      <Container>
-        <Separator height={50} />
-
-        <h2 style={{ paddingLeft: 30 }}>{data?.platform?.attributes.title}</h2>
-
-        {hasProducts ? (
-          <>
-            <GridGames games={data.games} />
-            <Separator height={30} />
-            <Pagination
-              currentPage={data?.pagination.page}
-              totalPages={data?.pagination.pageCount}
-            />
-          </>
-        ) : (
-          <NoResult
-            text={`La categoria ${
-              data?.platform?.attributes.title ?? params.platform
-            } aun no tiene productos`}
-          />
-        )}
-
-        <Separator height={100} />
-      </Container>
-    </BasicLayout>
+    <>
+      <PlatformPage data={data} params={params} />
+    </>
   );
 };
 
-export default PlatformPage;
+export default page;
